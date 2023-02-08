38e299047eb4f87284c3e81a65de91b0e18f984f
Nguyễn Viết Hiếu <hieuconstructor@gmail.com>
2023-02-08 14:09:49 +0700

    Sắp xếp tăng dần của Insert sort

---
+++ Sort-algorithm/Insertion-sort/Sort_up.dart
@@ -0,0 +1,30 @@
+void main(List<String> args) {
+  List<int> numbers = [0, 10, 5, 15, 20, 35, 25, 40, 30];
+
+  insertSort(numbers);
+  printListNumber(numbers);
+}
+
+// Insertion sort
+// Sắp xếp từ nhỏ đến lớn
+insertSort(List<int> numbers) {
+  int i, j, temp;
+  for (i = 1; i < numbers.length; i++) {
+    j = i - 1;
+    temp = numbers[i];
+    while (numbers[j] > temp && j >= 0) {
+      numbers[j + 1] = numbers[j];
+      j--;
+    }
+    numbers[j + 1] = temp;
+  }
+  return numbers;
+}
+
+//Ham in mang
+void printListNumber(List<int> numbers) {
+  int i;
+  for (i = 0; i < numbers.length; i++) {
+    print(numbers[i]);
+  }
+}
